6e9141f12a7e27cbc8aebd1788ac5e20f85907bb
JohnScreamer <bogaturscreamer@gmail.com>
2023-06-09 17:56:23 +0300

    kays fix

--- app/api/route.ts
+++ app/api/route.ts
@@ -2,7 +2,39 @@ import { NextResponse } from "next/server";
 import mongoose from "mongoose";
 import Playlist from "./models/PlaylistModel";
 import { VideoType } from "../../src/Redux/Slice/common";
+const getImg = (data: Thumbnails) => {
+    return (
+        data?.medium?.url ||
+        data?.maxres?.url ||
+        data?.high?.url ||
+        data?.standard?.url ||
+        data?.default?.url
+    );
+};
 
+const getDataFromYoutube = async (vidId: string, pageToken?: string) => {
+    console.log("youtube");
+
+    const str = pageToken ? `&pageToken=${pageToken}` : "";
+
+    const url = `https://youtube-v31.p.rapidapi.com/playlistItems?playlistId=${vidId}&part=snippet&maxResults=50${str}`;
+    const options = {
+        method: "GET",
+        headers: {
+            "X-RapidAPI-Key":
+                "2dc2afa31bmsh1c710e8569af426p13ed11jsn5f1b37776b28",
+            "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
+        },
+    };
+    try {
+        const response = await fetch(url, options);
+        const result = await response.text();
+
+        return JSON.parse(result) as ResponseType;
+    } catch (error) {
+        return null;
+    }
+};
 type allData = {
     totalAmount: number;
     list: Array<VideoType>;
@@ -77,30 +109,6 @@ const setPlaylist = async (obj: allData) => {
     return newPlaylist;
 };
 
-export const getDataFromYoutube = async (vidId: string, pageToken?: string) => {
-    console.log("youtube");
-
-    const str = pageToken ? `&pageToken=${pageToken}` : "";
-
-    const url = `https://youtube-v31.p.rapidapi.com/playlistItems?playlistId=${vidId}&part=snippet&maxResults=50${str}`;
-    const options = {
-        method: "GET",
-        headers: {
-            "X-RapidAPI-Key":
-                "2dc2afa31bmsh1c710e8569af426p13ed11jsn5f1b37776b28",
-            "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
-        },
-    };
-    try {
-        const response = await fetch(url, options);
-        const result = await response.text();
-
-        return JSON.parse(result) as ResponseType;
-    } catch (error) {
-        return null;
-    }
-};
-
 type ResponseType = {
     kind: string;
     nextPageToken?: string;
@@ -130,16 +138,6 @@ export interface Snippet {
     videoOwnerChannelTitle: string;
     videoOwnerChannelId: string;
 }
-
-const getImg = (data: Thumbnails) => {
-    return (
-        data?.medium?.url ||
-        data?.maxres?.url ||
-        data?.high?.url ||
-        data?.standard?.url ||
-        data?.default?.url
-    );
-};
 
 export interface Thumbnails {
     default: Default;
